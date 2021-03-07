bc434ca18de360d81fb11b4704c0f0e7ff6ebb95
Xoadra <xijp77@gmail.com>
2021-03-06 20:28:17 -0800

    Provided initial state to ViewArea for setting up WebGL

--- src/components/ViewArea.js
+++ src/components/ViewArea.js
@@ -1,14 +1,24 @@
+import { useRef, useState, useEffect } from 'react';
+
 import './ViewArea.css';
 
 
 function ViewArea() {
+    const canvas = useRef(null);
+    const [webGL, setWebGL] = useState(null);
+    const [message, setMessage] = useState('');
+    useEffect(() => {
+        setWebGL(() => {
+            const newWebGL = canvas.current.getContext('webgl');
+            setMessage(newWebGL ? 'Content coming soon...' : 'WebGL won\'t work! So sad...');
+            return newWebGL;
+        });
+    }, []);
     return (
         <figure>
             <div id="overlay"/>
-            <figcaption>
-                Content coming soon...
-            </figcaption>
-            <canvas/>
+            <figcaption>{message}</figcaption>
+            <canvas ref={canvas}/>
         </figure>
     );
 }
