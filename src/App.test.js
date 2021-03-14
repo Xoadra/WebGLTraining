3577ebb1e81ef163e5dad3339cbf64a264540d24
Xoadra <xijp77@gmail.com>
2021-03-13 18:52:35 -0800

    Enhanced and added additional tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 
 import App from './App';
 
 
-test('renders WebGL Fundamentals link', () => {
-    render(<App/>);
-    const linkElement = screen.getByText(/webgl fundamentals/i);
+beforeEach(() => render(<App/>));
+
+
+test('renders WebGL Training App link', () => {
+    const name = /webgl training app/i;
+    const headerElement = screen.getByRole('banner');
+    const linkElement = within(headerElement).getByRole('link', { name });
     expect(linkElement).toBeInTheDocument();
+});
+
+test('renders WebGL Fundamentals link', () => {
+    const name = /webgl fundamentals/i;
+    const footerElement = screen.getByRole('contentinfo');
+    const linkElement = within(footerElement).getByRole('link', { name });
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute('target', '_blank');
+    expect(linkElement).toHaveAttribute('rel', 'noopener noreferrer');
+});
+
+test('renders MDN Web Docs link', () => {
+    const name = /mdn web docs/i;
+    const footerElement = screen.getByRole('contentinfo');
+    const linkElement = within(footerElement).getByRole('link', { name });
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute('target', '_blank');
+    expect(linkElement).toHaveAttribute('rel', 'noopener noreferrer');
 });
