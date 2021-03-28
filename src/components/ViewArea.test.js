252ddf8591a08ffbad8c38371579ac5f7f319716
Xoadra <xijp77@gmail.com>
2021-03-27 20:59:48 -0700

    Altered ViewArea test to actually test for no figcaption text on initial render

--- src/components/ViewArea.test.js
+++ src/components/ViewArea.test.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import { render, screen, within } from '@testing-library/react';
 
 import ViewArea from './ViewArea';
 
 
-test('renders without any figcaption text', () => {
-    render(<ViewArea/>)
-    const pattern = /(a-z)*/i;
+test('renders without figcaption text before loading webgl', () => {
+    const mockUseEffect = jest.spyOn(React, 'useEffect').mockImplementation();
+    render(<ViewArea/>);
+    mockUseEffect.mockRestore();
     const figureElement = screen.getByRole('figure');
-    const captionElement = within(figureElement).getByText(pattern, { selector: 'figcaption' });
-    expect(captionElement).toBeInTheDocument();
+    const captionElement = within(figureElement).getByText(/.*/, { selector: 'figcaption' });
+    expect(captionElement).toBeEmptyDOMElement();
 });
