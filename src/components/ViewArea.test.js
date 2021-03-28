b412f145733db831a0ffe19a37584bbc477f3c26
Xoadra <xijp77@gmail.com>
2021-03-27 21:14:18 -0700

    Third ViewArea test added to test figcaption text with WebGL availability

--- src/components/ViewArea.test.js
+++ src/components/ViewArea.test.js
@@ -19,3 +19,20 @@ test('renders figcaption error text if webgl is unavailable', () => {
     const captionElement = within(figureElement).getByText(/.*/, { selector: 'figcaption' });
     expect(captionElement).toHaveTextContent(/webgl won't work! so sad.../i);
 });
+
+test('renders figcaption content status text if webgl is available', () => {
+    const mockGetContext = jest.spyOn(HTMLCanvasElement.prototype, 'getContext');
+    mockGetContext.mockReturnValue({
+        compileShader: jest.fn(),
+        createShader: jest.fn(),
+        deleteShader: jest.fn(),
+        getShaderInfoLog: jest.fn(shader => 'Info'),
+        getShaderParameter: jest.fn((shader, status) => true),
+        shaderSource: jest.fn()
+    });
+    render(<ViewArea/>);
+    mockGetContext.mockRestore();
+    const figureElement = screen.getByRole('figure');
+    const captionElement = within(figureElement).getByText(/.*/, { selector: 'figcaption' });
+    expect(captionElement).toHaveTextContent(/content coming soon.../i);
+});
